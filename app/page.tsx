0de8a8dc56993925620be8b517d7ed887988b89a
Tha Lynn Htet <thalynnhtet2020@gmail.com>
2025-10-04 13:52:13 -0700

    test 3

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,6 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">hiii mofos</main>
+    <main className="min-h-screen flex flex-col items-center">hiii mofos!!!!!</main>
   );
 }
