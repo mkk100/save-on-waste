031457639dddfc94e8952cb6276d498d5e56df2f
Myo Min Khant <mmkblockbaster@gmail.com>
2025-10-05 02:55:45 -0700

    Dashboard(before)

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,22 @@ const LazyMap = dynamic(() => import("@/components/Map"), {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-200">
-      <LazyMap />
-    </main>
+    <>
+      <header className="w-full bg-emerald-600 text-white fixed top-0 left-0 z-20 h-16 grid grid-cols-3 items-center px-6">
+        <h1 className="text-lg font-semibold justify-self-start">
+          Save on Waste
+        </h1>
+        <h2 className="text-lg justify-self-center">Map</h2>
+        <img
+          src="/favicon.ico"
+          alt="Account Logo"
+          className="w-12 h-12 rounded-md object-cover justify-self-end"
+        />
+      </header>
+
+      <main className="min-h-screen pt-16 p-6 bg-slate-200">
+        <LazyMap />
+      </main>
+    </>
   );
 }
