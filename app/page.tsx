c281156ea012cf168a37620cb11c1b0bde2fc7b7
mkk100 <tommychimes2003@gmail.com>
2025-10-04 18:49:50 -0700

    feat: add interactive map component with Leaflet integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/server";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
+
+import dynamic from "next/dynamic";
+const DynamicMapComponent = dynamic(() => import("./Map"), { ssr: !!false });
 
 export default async function Instruments() {
   const supabase = await createClient();
   const { data: instruments } = await supabase.from("businesses").select();
-
   return (
     <pre>
       {JSON.stringify(instruments, null, 2)}
       <Button>Hello World</Button>
+      <DynamicMapComponent />
     </pre>
   );
 }
