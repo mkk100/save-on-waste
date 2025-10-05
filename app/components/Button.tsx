ebefcd3979f52e17348e5a8b2474d8ab956bcd05
Tha Lynn Htet <thalynnhtet2020@gmail.com>
2025-10-04 18:15:39 -0700

    added modal content

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Modal, Box, Typography } from '@mui/material';
+import { Button, Modal, Box, Typography, Slide, TextField } from '@mui/material';
 import { useState } from 'react';
 
 const modalStyle = {
@@ -13,10 +13,14 @@ const modalStyle = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
+  outline: 'none', // Remove focus outline
 };
 
 export default function UploadButton() {
   const [open, setOpen] = useState(false);
+  const [businessName, setBusinessName] = useState('');
+  const [foodCategory, setFoodCategory] = useState('');
+  const [foodWeight, setFoodWeight] = useState('');
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -35,16 +39,39 @@ export default function UploadButton() {
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
-        <Box sx={modalStyle}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Upload File
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Select a file to upload to the system.
-          </Typography>
-          {/* Add your upload form content here */}
-        </Box>
+        <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+          <Box sx={modalStyle}>
+
+            <Typography id="modal-modal-title" variant="h6" component="h2" color='black' sx={{ mb: 3 }}>
+                Upload Food Details
+            </Typography>
+
+              <Typography variant="body1">Category of food:</Typography>
+              <TextField
+                fullWidth
+                variant="outlined"
+                value={foodCategory}
+                onChange={(e) => setFoodCategory(e.target.value)}
+                placeholder="Enter food category"
+              />
+              
+              <Typography variant="body1">Weight of food (kgs):</Typography>
+              <TextField
+                fullWidth
+                variant="outlined"
+                type="number"
+                value={foodWeight}
+                onChange={(e) => setFoodWeight(e.target.value)}
+                placeholder="Enter weight in kg"
+              />
+            </Box>
+        </Slide>
       </Modal>
     </>
   );
