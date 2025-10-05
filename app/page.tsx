3b39a896d2835739b12f721e503e65cb2dee7a1b
mkk100 <tommychimes2003@gmail.com>
2025-10-04 17:12:49 -0700

    chore: add Material-UI and Emotion dependencies for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/server";
 
 export default async function Instruments() {
   const supabase = await createClient();
   const { data: instruments } = await supabase.from("businesses").select();
 
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+  return (
+    <pre>
+      {JSON.stringify(instruments, null, 2)}
+      <Button>Hello World</Button>
+    </pre>
+  );
 }
